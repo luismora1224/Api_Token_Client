ba1ee476b66b7edec43ed4982dde6ca9e323b2fc
Luis Enrique Mora <dark_100@live.com>
2024-09-10 13:11:55 -0600

    Se agrega la funcionalidad para poder consumir el microservicio con seguridad, utilizando un jwt

---
+++ src/main/java/com/mexisoft/jwt_client/auth/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.mexisoft.jwt_client.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests( authHttp -> authHttp
+				.requestMatchers(HttpMethod.GET,"/authorized").permitAll()
+				.requestMatchers(HttpMethod.GET, "/list").hasAnyAuthority("SCOPE_read", "SCOPE_write")
+				.requestMatchers(HttpMethod.POST, "/create").hasAuthority("SCOPE_write")
+				.anyRequest().authenticated())
+		.csrf(csrf -> csrf.disable()) // se desactiva la proteccion csrf que se usa para formularios
+		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		.oauth2Login(login -> login.loginPage("/oauth2/authorization/client-app"))
+		.oauth2Client(withDefaults())
+		.oauth2ResourceServer(resourceServer -> resourceServer.jwt(withDefaults()));
+		
+		return http.build();
+	}
+}
